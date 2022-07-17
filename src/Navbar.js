17ae7fa8fcfc2371e702d7dcce9106369b63bd30
ibktommy <atomquartz@gmail.com>
2022-07-17 22:33:04 +0100

    Adding state to display links on mobile view when menu bar is clicked

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from './logo.svg'
-import { FaBars, FaFacebook, FaLinkedin, FaBehance, FaTwitter } from 'react-icons/fa'
+import { FaBars } from 'react-icons/fa'
 import { links, social } from './data'
 
 const Navbar = () => {
+  // Setting Navabr State
+  const [showLinks, setShowLinks] = useState(false)
+
   return (
     <>
       <nav>
@@ -27,25 +30,27 @@ const Navbar = () => {
             </ul>
           </div>
 
-          <button className="nav-toggle hidden">
+          <button className="nav-toggle" onClick={() => setShowLinks(!showLinks)}>
             <FaBars />
           </button>
         </div>
 
-        <div className="links-container show-container">
-          <ul className="links">
-            {
-              links.map((link) => {
-                const { id, text, url } = link
-                return (
-                  <li key={id}>
-                    <a href={url} className="link">{text}</a>
-                  </li>
-                )
-              })
-            }
-          </ul>
-        </div>
+        {
+          showLinks && <div className="links-container show-container">
+            <ul className="links">
+              {
+                links.map((link) => {
+                  const { id, text, url } = link
+                  return (
+                    <li key={id}>
+                      <a href={url} className="link">{text}</a>
+                    </li>
+                  )
+                })
+              }
+            </ul>
+          </div>
+        }
 
         <ul className="social-icons">
           {
